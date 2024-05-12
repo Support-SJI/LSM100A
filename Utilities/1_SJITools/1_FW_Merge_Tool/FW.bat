517a0110b5b12225522425c71e293b43ffe44332
JIN-MIRAE <oggi@seongji.co.kr>
2024-05-13 08:48:56 +0900

    Created
    LSM100A Created

--- Utilities/1_SJITools/1_FW_Merge_Tool/FW.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo bootloader_name app_name fw_name
-echo fw start
-srec_cat.exe %1 -Intel %2 -Intel -o %3 -Intel
-echo end
--- Utilities/1_SJITools/1_FW_Merge_Tool/FW_Merge_SDK.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo fw merge start
-FW.bat LSM_IAP_V100_220516.hex LSM1x0A_SDK_LoRaWAN_Sigfox.hex LSM1x0A_V100_SDK_FW.hex
-echo end
